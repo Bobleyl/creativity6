8c841d02fcc154cbee7c9dc3ac6b8f5f5a68743e
EC2 Default User <ec2-user@ip-172-31-38-176.ec2.internal>
2018-12-11 20:57:45 +0000

    it lives

--- public/javascripts/my_app.js
+++ public/javascripts/my_app.js
@@ -2,22 +2,25 @@ angular.module('myApp', []).
   controller('myController', ['$scope', '$http',
                               function($scope, $http) {
     $http.get('/user/profile')
-        .then(function(data, status, headers, config) {
-      $scope.user = data;
+    .then(function(data, status, headers, config){
+      console.log("INSIDE USER/PROFILE:")
+      console.log(data)
+       $scope.user = data.data;
       $scope.error = "";
-    }).then(function() {
       $scope.users = [];
-      $http.get('/users')
-        .then(function(data, status, headers, config) {
+      return $http.get('/users')
+    })
+    .then(function(data, status, headers, config) {
           $scope.users = data['data'];
-          
+          console.log(data);
           var i;
           for(i = 0; i < $scope.users.length; i++) {
             var user = $scope.users[i];
-            if(user._id == $scope.user['data']._id) {
+            if(user._id == $scope.user._id) {
               $scope.users.splice(i, 1);
             }
           }
       });
-    })
 }]);
+
+//
